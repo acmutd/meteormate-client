2922a6bc957cffd65981cd16ce16dfa1dea298cc
Aastha Sheth <108955384+aasthas23@users.noreply.github.com>
2025-06-30 14:53:40 -0500

    web app template made

---
+++ meteor-mate/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Meteormate",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
