dc74cb2246ff11e668a69c57cb78e24b17330e41
Nathan <nathan.sujatno@gmail.com>
2025-10-24 15:08:25 -0500

    disallowed mobile view (#16)
    * disallowed mobile view
    
    * added styling to the screen blocker
    
    * updated styling
    
    * tiny centering fix
    
    ---------
    
    Co-authored-by: ryanpolasky <ryanpolasky@hotmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
 "use client";
 
 import "./globals.css";
-import { AuthProvider } from "../contexts/authContext"; // adjust the path if your folder is named `contexts/authContext`
+import { AuthProvider } from "../contexts/authContext";
+import ScreenBlocker from "../../components/ScreenBlocker";
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="m-0 p-0">
-        <AuthProvider>
-          {children}
-        </AuthProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className="m-0 p-0">
+				<AuthProvider>
+					<div className="hidden md:block">{children}</div>
+					<div className="block md:hidden">
+						<ScreenBlocker />
+					</div>
+				</AuthProvider>
+			</body>
+		</html>
+	);
 }
