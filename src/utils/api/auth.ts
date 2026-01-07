65c9b671e2914f2796fd04b60aa772f9454749cf
Atharva Mishra <atharvamishra956+1@gmail.com>
2026-01-06 22:39:55 -0600

    added function to fetch the register route on the backend

---
+++ src/utils/api/auth.ts
@@ -0,0 +1,32 @@
+import { MMApiError, Result, UserRegisterResponse } from "../types";
+
+async function callRegisterRoute(email: string, password: string, utd_id: string): Promise<Result<UserRegisterResponse>> {
+    try {
+        const response = await fetch(`${process.env.API_BASE_URL}/auth/register`, {
+            'method': 'POST',
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+            'body': JSON.stringify({
+                email,
+                password,
+                utd_id
+            })
+        });
+
+        if (!response.ok) {
+            const data = (await response.json()) as MMApiError
+            return { ok: false, error: data.detail, code: response.status }
+        }
+
+        const data = (await response.json()) as UserRegisterResponse
+
+        return { ok: true, data }
+    } catch (error) {
+        return {
+            ok: false,
+            error: error instanceof Error ? error.message : "Internal Server Error",
+            code: 500
+        }
+    }
+}
