fb507edea061e86e6bf0ea928781f98d02acb607
Atharva Mishra <atharvamishra956+1@gmail.com>
2026-01-06 22:45:52 -0600

    forgot about the export

--- src/utils/api/auth.ts
+++ src/utils/api/auth.ts
@@ -1,6 +1,6 @@
 import { MMApiError, Result, UserRegisterResponse } from "../types";
 
-async function callRegisterRoute(email: string, password: string, utd_id: string): Promise<Result<UserRegisterResponse>> {
+export async function callRegisterRoute(email: string, password: string, utd_id: string): Promise<Result<UserRegisterResponse>> {
     try {
         const response = await fetch(`${process.env.API_BASE_URL}/auth/register`, {
             'method': 'POST',
