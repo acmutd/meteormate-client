f551b3506bfa7829d914fd8737f81a474c436476
Nathan <nathan.sujatno@gmail.com>
2026-01-07 17:56:03 -0600

    initial integration complete, updated values to match what backend expects, currently using put request because there's an issue with deleting a survey from the database

---
+++ src/app/onboarding/housing/page.tsx
@@ -0,0 +1,358 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import LifestylePreferencesCard from "../../../../components/LifestylePreferencesCard";
+import DoneButton from "../../../../components/DoneButton";
+import ProgressHeader from "../../../../components/ProgressHeader";
+import React, { useMemo } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { loadOnboardingData, updateOnboardingData, clearOnboardingData } from "@/utils/onboardingStorage";
+import { truncate } from "fs/promises";
+import { getAuth } from "firebase/auth";
+
+const sendOnboardingData = async () => {
+	const data = loadOnboardingData();
+	const body = JSON.stringify(data)
+	const auth = getAuth();
+	const user = auth.currentUser;
+	if (user) {
+		const token = await user.getIdToken();
+		await fetch("http://localhost:8000/api/survey", {
+			method: "PUT",
+			headers: { "Content-Type": "application/json",
+				"Authorization": `Bearer ${token}`
+			},
+			body: JSON.stringify(data),
+		});
+
+		clearOnboardingData();
+	}
+	else{
+		console.log("user not found")
+	}
+	console.log(body)
+}
+
+function OnCampusUI() {
+	const router = useRouter();
+	const handleNextStep = () => {
+		sendOnboardingData()
+		// router.push("/onboarding/dashboard");
+	};
+
+    const [selectedLocation, setSelectedLocation] = useState<string[]>([]);
+    const [selectedHonorsStatus, setSelectedHonorsStatus] = useState<boolean | null>(
+        null
+    );
+    const [selectedLLCPreference, setSelectedLLCPreference] = useState<boolean | null>(
+		null
+	);
+
+	const [hydrated, setHydrated] = useState(false);
+	
+	useEffect(() => { // loading it once and the next use effect for the changes made and keeping track
+		const saved = loadOnboardingData();
+		if (Array.isArray(saved.on_campus_locations)) {
+			setSelectedLocation(saved.on_campus_locations);
+		}
+		setSelectedHonorsStatus(saved.honors ?? null);
+		setSelectedLLCPreference(saved.llc_interest ?? null);
+
+		setHydrated(true);
+	}, []);
+
+	useEffect(() => {
+	if (!hydrated) return;
+		updateOnboardingData({
+			on_campus_locations: selectedLocation,
+			honors: selectedHonorsStatus,
+			llc_interest: selectedLLCPreference,
+		});
+	}, [
+		hydrated,
+		selectedLocation,
+		selectedHonorsStatus,
+		selectedLLCPreference,
+	]);
+
+	const handleLocationToggle = (value: string) => {
+		if (selectedLocation.includes(value)) {
+			setSelectedLocation(selectedLocation.filter((item) => item !== value));
+		} else {
+			setSelectedLocation([...selectedLocation, value]);
+		}
+		};
+
+	const handleBooleanToggle = (
+		currentValue: boolean | null,
+		setValue: (val: boolean | null) => void,
+		newValue: boolean
+	) => {
+		if (currentValue === newValue) {
+			setValue(null);
+		} else {
+			setValue(newValue);
+		}
+	};
+
+    return (
+        <div>
+            <ProgressHeader 
+				title="On Campus"
+				subtitle="Help us find your ideal roommate by selecting your preferences!"
+				currentStep={5}
+			/>
+			<div className="py-8 px-15 w-full flex flex-col">
+				<p className="text-black text-sm font-bold mt-1 mb-2">
+					Location: Northside, UV, CC, or Freshmen dorms?
+				</p>
+				{/*grid for the 3 options*/}
+				<div className="grid grid-cols-4 gap-2 mb-4 cursor-pointer">
+					<LifestylePreferencesCard
+						title="Northside"
+						imageSrc="/images/houses.webp" //need to change this
+						isSelected={selectedLocation.includes("northside")}
+						onClick={() => handleLocationToggle("northside")}
+					/>
+					<LifestylePreferencesCard
+						title="University Village"
+						imageSrc="/images/houses.webp" //need to change this
+						isSelected={selectedLocation.includes("uv")}
+						onClick={() => handleLocationToggle("uv")}
+					/>
+					<LifestylePreferencesCard
+						title="Canyon Creek"
+						imageSrc="/images/houses.webp" //need to change this
+						isSelected={selectedLocation.includes("cc")}
+						onClick={() => handleLocationToggle("cc")}
+					/>
+					<LifestylePreferencesCard
+						title="Freshmen Dorms"
+						imageSrc="/images/3D-buildings.webp" // change this
+						isSelected={selectedLocation.includes("freshman_dorms")}
+						onClick={() => handleLocationToggle("freshman_dorms")}
+					/>
+					
+				</div>
+				
+				<p className="text-black text-sm font-bold mt-1 mb-2">
+					Are you an Honors student?
+				</p>
+				<div className="grid grid-cols-2 gap-4 mb-4 cursor-pointer"> 
+					<LifestylePreferencesCard
+						title="Yes, I am an honors student"
+						imageSrc="/images/badge.webp"
+						isSelected={selectedHonorsStatus === true}
+						onClick={() => handleBooleanToggle(selectedHonorsStatus, setSelectedHonorsStatus, true)}
+					/>
+					<LifestylePreferencesCard
+						title="No, I am not an honors student"
+						imageSrc="/images/study.webp"
+						isSelected={selectedHonorsStatus === false}
+						onClick={() => handleBooleanToggle(selectedHonorsStatus, setSelectedHonorsStatus, false)}
+					/>
+					
+				</div>
+				
+				<p className="text-black text-sm mt-1 font-bold mb-2">
+					Are you interested in being part of the Living Learning Community?
+				</p>
+				<div className="grid grid-cols-2 gap-4 mb-4 cursor-pointer">
+					<LifestylePreferencesCard
+						title="Yes, I would like to be a part of LLC"
+						imageSrc="/images/environment.webp"
+						isSelected={selectedLLCPreference === true}
+						onClick={() => handleBooleanToggle(selectedLLCPreference, setSelectedLLCPreference, true)}
+					/>
+					<LifestylePreferencesCard
+						title="No, I would not like to be a part of LLC"
+						imageSrc="/images/reading-book.webp"
+						isSelected={selectedLLCPreference === false}
+						onClick={() => handleBooleanToggle(selectedLLCPreference, setSelectedLLCPreference, false)}
+					/>
+					
+				</div>
+            </div>
+			<div className="flex items-center justify-center">
+            <DoneButton
+                                className="mt-7"
+                                logo={<img src="/images/peechi_duo.webp" />}
+                                onClick={handleNextStep}
+								disabled = {selectedLLCPreference === null || selectedHonorsStatus === null || !selectedLocation}
+								
+                            />
+    		</div>
+        </div>
+  );
+}
+
+function OffCampusUI() {
+	const router = useRouter();
+	const handleNextStep = async () => {
+		sendOnboardingData();
+		// router.push("/onboarding/dashboard");
+	};
+
+    const [selectedLeaseStatus, setSelectedLeaseStatus] = useState<boolean | null>(
+        null
+    );
+    const [selectedHaveLeaseLength, setSelectedHaveLeaseLength] = useState<string | null>(
+		null
+	);
+
+	const [hydrated, setHydrated] = useState(false);
+	
+	useEffect(() => { // loading it once and the next use effect for the changes made and keeping track
+		const saved = loadOnboardingData();
+		setSelectedLeaseStatus(saved.have_lease ?? null);
+		setSelectedHaveLeaseLength(saved.have_lease_length ?? null);
+
+		setHydrated(true);
+	}, []);
+
+	useEffect(() => {
+	if (!hydrated) return;
+		updateOnboardingData({
+			have_lease: selectedLeaseStatus,
+			have_lease_length: selectedHaveLeaseLength,
+		});
+	}, [
+		hydrated,
+		selectedLeaseStatus,
+		selectedHaveLeaseLength,
+	]);
+
+    const handleToggle = (
+		currentValue: string | null,
+		setValue: (val: string | null) => void,
+		newValue: string
+	) => {
+		if (currentValue === newValue) {
+      setValue(null);
+    } else {
+      setValue(newValue);
+    }
+	}
+
+	const handleBooleanToggle = (
+		currentValue: boolean | null,
+		setValue: (val: boolean | null) => void,
+		newValue: boolean
+	) => {
+		if (currentValue === newValue) {
+			setValue(null);
+		} else {
+			setValue(newValue);
+		}
+	};
+
+  return (
+    <div>
+            <ProgressHeader 
+				title="Off Campus"
+				subtitle="Help us find your ideal roommate by selecting your preferences!"
+				currentStep={5}
+			/>
+			<div className="py-8 px-15 w-full flex flex-col">
+				<h1 className="text-black text-xl font-bold">Do you have a lease?</h1>
+                <p className="text-black text-sm mb-3 mt-2">
+					This will help us pair you with the right people.
+				</p>
+				{/*grid for the 3 options*/}
+				<div className="grid grid-cols-2 gap-2 mb-4 cursor-pointer">
+					<LifestylePreferencesCard
+						title="I have a lease and need a roommate"
+						imageSrc="/images/houses.webp" //need to change this
+						isSelected={selectedLeaseStatus === true}
+						onClick={() => handleBooleanToggle(selectedLeaseStatus, setSelectedLeaseStatus, true)}
+					/>
+					<LifestylePreferencesCard
+						title="I do not have a lease"
+						imageSrc="/images/3D-buildings.webp" // change this
+						isSelected={selectedLeaseStatus === false}
+						onClick={() => handleBooleanToggle(selectedLeaseStatus, setSelectedLeaseStatus, false)}
+					/>
+					
+				</div>
+				
+				<h1 className="text-black text-xl font-bold">Have a lease: </h1>
+                <p className="text-black text-sm mb-3 mt-2">
+					Pick the option that best matches how long the roommate would stay.
+				</p>
+				<div className="grid grid-cols-3 gap-4 mb-4 cursor-pointer"> 
+					<LifestylePreferencesCard
+						title="Semester"
+						imageSrc="/images/badge.webp"
+						isSelected={selectedHaveLeaseLength === "semester"}
+						onClick={() => handleToggle(selectedHaveLeaseLength, setSelectedHaveLeaseLength, "semester")}
+					/>
+					<LifestylePreferencesCard
+						title="Academic Year"
+						imageSrc="/images/study.webp"
+						isSelected={selectedHaveLeaseLength === "academic_year"}
+						onClick={() => handleToggle(selectedHaveLeaseLength, setSelectedHaveLeaseLength, "academic_year")}
+					/>
+					<LifestylePreferencesCard
+						title="Year"
+						imageSrc="/images/study.webp"
+						isSelected={selectedHaveLeaseLength === "year"}
+						onClick={() => handleToggle(selectedHaveLeaseLength, setSelectedHaveLeaseLength, "year")}
+					/>
+					
+				</div>
+				
+				
+            </div>
+			<div className="flex items-center justify-center">
+            <DoneButton
+                                className="mt-7"
+                                logo={<img src="/images/peechi_duo.webp" />}
+                                onClick={handleNextStep}
+								disabled = {!selectedHaveLeaseLength || !selectedLeaseStatus}
+								
+                            />
+    		</div>
+        </div>
+  );
+}
+
+export default function HousingPage() {
+
+ 	const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const living = useMemo(
+    () => searchParams.get("living") ?? "",
+    [searchParams]
+  );
+
+	useEffect(() => {
+		const saved = loadOnboardingData()
+		console.log(saved)
+	})
+  
+  return (
+    <div className="p-6">
+      {living === "on_campus" ? (
+        <OnCampusUI />
+      ) : living === "off_campus" ? (
+        <OffCampusUI />
+      ) : (
+        <div>
+          <p className="text-red-600 text-sm">
+            No living preference passed. Please go back and select one.
+          </p>
+          <button
+            className="mt-4 px-4 py-2 rounded-lg border"
+            onClick={() => router.back()}
+          >
+            Go back
+          </button>
+        </div>
+
+        
+      )}
+      
+    </div>
+  );
+}
