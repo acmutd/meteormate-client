94793f48b8138fa7466949b2b8227b0934659c9d
Justin Truong <justintruong222@gmail.com>
2026-01-12 14:06:35 -0600

    chore: sync with fetch json data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,16 +3,13 @@ import React, { useEffect, useState } from "react";
 import { getCurrentUserIdToken } from "@/firebase/auth";
 import { useRouter } from "next/navigation";
 
-// Following "class UserResponse(BaseModel):" schema from backend
 type UserProfile = {
   id: string;
   email: string;
-  // first_name: string;
-  // last_name: string;
-  // age?: number;
-  // birthdate?: Date;
-  created_at?: Date;
-  utd_id?: string;
+  utd_id: string;
+  created_at: Date;
+  survey_done: boolean;
+  profile_created: boolean;
 };
 
 export default function Profile() {
@@ -26,7 +23,7 @@ export default function Profile() {
     const fetchuser = async () => {
       try {
         const token = await getCurrentUserIdToken();
-        const res = await fetch("http://127.0.0.1:8000/api/auth/me", { // Todo: Change this
+        const res = await fetch("http://127.0.0.1:8000/api/auth/me", {
           method: "GET",
           headers: { Authorization: `Bearer ${token}` },
         });
@@ -38,27 +35,26 @@ export default function Profile() {
       } catch (err) {
         console.error("Profile fetch error:", err);
         setError("Failed to load profile");
-        router.push("/authentication?toast=not-signed-in")
+        router.push("/authentication?toast=not-signed-in");
       } finally {
         setLoading(false);
       }
     };
     fetchuser();
-  }, []);
+  }, [router]);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
   if (!user) return <div>No user data found</div>;
-  const userData = user.user;
+  const userData = user;
 
   return (
-    // Testing response
     <div className="min-h-screen w-screen flex flex-col justify-center items-center relative">
       <div className="w-[60%] min-h-180 flex flex-col items-center bg-[#EEE5D8] rounded-2xl shadow-md shadow-gray-700">
         <img
           src={
-            userData.profile_image
-              ? userData.profile_image
+            userData.profile?.profile_picture_url
+              ? userData.profile?.profile_picture_url
               : "images/peechi_duo.png"
           }
           alt="Profile Picture"
@@ -69,13 +65,14 @@ export default function Profile() {
           <div className="text-md">
             Name
             <div className="bg-white rounded shadow h-8 w-full overflow-hidden flex items-center text-center text-black">
-              <p className="ml-2"> {/* Missing from API */}
-                {userData.first_name
-                  ? userData.first_name
+              <p className="ml-2">
+                {/* {userData.profile?.first_name
+                  ? userData.profile?.first_name
                   : "(First Name Not Provided)"}{" "}
-                {userData.last_name
-                  ? userData.last_name
-                  : "(Last Name Not Provided)"}{" "}
+                {userData.profile?.last_name
+                  ? userData.profile.last_name
+                  : "(Last Name Not Provided)"}{" "} */}
+                (Placeholder)
               </p>
             </div>
           </div>
@@ -110,22 +107,50 @@ export default function Profile() {
           </div>
 
           <div className="text-md">
-            ID
-            <div className="bg-white rounded shadow h-8 w-full overflow-hidden flex items-center text-center text-black justify-between">
-              <p className={`ml-2 ${showId ? "" : "blur"}`}>{userData.id ? userData.id : "(Unknown)"}</p>
-              <button className="underline text-xs mr-2" onClick={() => setShowId(!showId)}>{showId ? "Hide" : "Show"}</button>
+            Created Profile?
+            <div className="bg-white rounded shadow h-8 w-full overflow-hidden flex items-center text-center text-black">
+              <p className="ml-2">
+                {userData.profile_created === true
+                  ? "Yes"
+                  : userData.profile_created === false
+                  ? "No"
+                  : "(Unknown)"}
+              </p>
             </div>
           </div>
 
           <div className="text-md">
             Completed Survey?
             <div className="bg-white rounded shadow h-8 w-full overflow-hidden flex items-center text-center text-black">
-              <p className="ml-2">{user.survey_done ? "Yes" : user.survey_done ? "No" : "(Unknown)"}</p> {/* Todo: Fix */}
+              <p className="ml-2">
+                {userData.survey_done === true
+                  ? "Yes"
+                  : userData.survey_done === false
+                  ? "No"
+                  : "(Unknown)"}
+              </p>
             </div>
-              
           </div>
+
+          <div className="text-md">
+            ID
+            <div className="bg-white rounded shadow h-8 w-full overflow-hidden flex items-center text-center text-black justify-between">
+              <p className={`ml-2 ${showId ? "" : "blur"}`}>
+                {userData.id ? userData.id : "(Unknown)"}
+              </p>
+              <button
+                className="underline text-xs mr-2"
+                onClick={() => setShowId(!showId)}
+              >
+                {showId ? "Hide" : "Show"}
+              </button>
+            </div>
+          </div>
+
+          <br></br>
+          <div>(User profile fields coming soon once create profile page is done)</div>
         </div>
-        {/* Debug: To see backend fetch response */}
+        {/* Debug: To see fetch response */}
         {/* <div>User data:</div> */}
         {/* <pre>{JSON.stringify(user, null, 2)}</pre> */}
       </div>
