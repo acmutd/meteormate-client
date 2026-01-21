842e1b8a1fb38dacc3ddf29d09733a5f09ec35d8
Ryan Polasky <ryanpolasky@hotmail.com>
2026-01-21 12:42:33 -0600

    relative imports

--- api/index.py
+++ api/index.py
@@ -9,9 +9,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
-from ..backend.config import settings
-from ..backend.database import engine, Base
-from ..backend.routes import auth, survey, matches, cron, profiles
+from backend.config import settings
+from backend.database import engine, Base
+from backend.routes import auth, survey, matches, cron, profiles
 
 # Logging setup
 logger = logging.getLogger("meteormate")
