45f0f2225ac8521a7b58d7fbea4f4152d4029d7f
Justin Truong <justintruong222@gmail.com>
2026-01-23 14:27:25 -0600

    created file for react easy crop

---
+++ src/components/ImageCropper.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Cropper from "react-easy-crop";
+
+interface ImageCropperProps {
+  image: string;
+  onCropComplete: (croppedArea: any, croppedAreaPixels: any) => void;
+}
+
+export default function ImageCropper({
+  image,
+  onCropComplete,
+}: ImageCropperProps) {
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+
+  return (
+    <div className="">
+        <Cropper
+          image={image}
+          crop={crop}
+          zoom={zoom}
+          aspect={1}
+          onCropChange={setCrop}
+          onZoomChange={setZoom}
+          onCropComplete={onCropComplete}
+        ></Cropper>
+    </div>
+  );
+}
+
+export async function getCroppedImg(imageSrc: string, pixelCrop: any): Promise<string> {
+  const image = new window.Image();
+  image.src = imageSrc;
+  await new Promise((resolve) => (image.onload = resolve));
+  const canvas = document.createElement("canvas");
+  canvas.width = pixelCrop.width;
+  canvas.height = pixelCrop.height;
+  const ctx = canvas.getContext("2d");
+  if (!ctx) throw new Error("No 2d context");
+  ctx.drawImage(
+    image,
+    pixelCrop.x,
+    pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height,
+    0,
+    0,
+    pixelCrop.width,
+    pixelCrop.height
+  );
+  return canvas.toDataURL("image/jpeg");
+}
