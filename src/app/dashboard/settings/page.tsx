d7583ee7f6865d1306acad27b0acd97342965152
Justin Truong <121770868+JuuyouSan@users.noreply.github.com>
2026-02-18 15:40:28 -0600

    didn't mean to push this file

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,8 +1,8 @@
 // the discover page - dashboard we are using to make sure that all the pages of the main app are here 
-export default function profile() {
+export default function settings() {
   return (
     <div>
-      <h2 className="text-2xl font-semibold">Profile</h2>
+      <h2 className="text-2xl font-semibold">Settings</h2>
       <p className="mt-2 text-sm text-gray-600">
         You’re logged in. This page is rendered inside the (app) layout, so the
         Navbar + Sidebar should always be visible.
