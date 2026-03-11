38b19af7b1c3521ebf8355f9aec9ee1c2023ae70
Justin Truong <justintruong222@gmail.com>
2026-03-11 02:19:32 -0500

    adjusted Update Proifle button padding on profile habits page

--- src/app/dashboard/profile/lifestyleHabits/page.tsx
+++ src/app/dashboard/profile/lifestyleHabits/page.tsx
@@ -128,7 +128,7 @@ export default function LifestyleHabitsPage() {
           <button
             type="button"
             onClick={handleUpdateProfile}
-            className="absolute right-6 top-2 px-6 py-2 rounded-lg text-black font-medium shadow bg-linear-60 from-[#F28C00] to-[#FFC243] hover:from-[#d97706] hover:to-[#f59e0b] hover:shadow-md transition-all duration-200"
+            className="absolute right-8 top-4 px-6 py-2 rounded-lg text-black font-medium shadow bg-linear-60 from-[#F28C00] to-[#FFC243] hover:from-[#d97706] hover:to-[#f59e0b] hover:shadow-md transition-all duration-200"
           >
             Update Profile
           </button>
